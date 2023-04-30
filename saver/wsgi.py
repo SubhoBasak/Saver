e6c1833b55ae0aa82bf06add5f127c5ffeb32ba4
SubhoBasak <subhobasak50@gmail.com>
2023-04-30 14:54:31 +0530

    debug

--- saver/wsgi.py
+++ saver/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/home/ubuntu/Saver/')
+sys.path.append('/home/ubuntu/Saver/venv/lib/python3.10/site-packages/')
 
 from django.core.wsgi import get_wsgi_application
 
