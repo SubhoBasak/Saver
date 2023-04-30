099e7f7fe21591fce2f762cf227e991bcd07da7f
SubhoBasak <subhobasak50@gmail.com>
2023-04-30 16:17:07 +0530

    debug

--- saver/settings.py
+++ saver/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'static_root/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo cp 000-default.conf /etc/apache2/sites-available/000-default.conf
 chmod 777 db.sqlite3
+python3 manage.py coll
 sudo chown :www-data db.sqlite3
 sudo chown :www-data ~/Saver
 sudo service apache2 restart
