3b6518246814d4357c30f029e4934f444b26a13d
SubhoBasak <subhobasak50@gmail.com>
2023-05-03 08:10:04 +0530

    config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo cp 000-default.conf /etc/apache2/sites-available/000-default.conf
 chmod 777 db.sqlite3
+sudo chmod 777 /home/ubuntu
 sudo chown :www-data db.sqlite3
 sudo chown :www-data ~/Saver
 sudo service apache2 restart
